faf14d47dd656bb4d8fa1c1b7b5dab605517ee15
Igor Yakushev <igor.yakushev@gmail.com>
2022-12-19 19:08:11 +0300

    Finish task 10

---
+++ Task_10/Program.cs
@@ -0,0 +1,24 @@
+﻿//получает число с консоли. "Защита от дурака"
+int GetNumberFromConsole(string message)
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.Write(message);
+
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число.");
+        }
+    }
+
+    return result;
+}
+
+int number = GetNumberFromConsole("Input three-digit number: ");
+Console.WriteLine((number - 100 * (number / 100)) / 10);
