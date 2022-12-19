f1725c9e0842a9a039296e0fb6a569a9f14b5a41
Igor Yakushev <igor.yakushev@gmail.com>
2022-12-19 17:34:36 +0300

    Finisk task 8

---
+++ Task_8/Program.cs
@@ -0,0 +1,29 @@
+﻿//получает число с консоли. "Защита от дурака"
+int GetNumberFromConsole(string message)
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.Write(message);
+
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число.");
+        }
+    }
+
+    return result;
+}
+
+int numberOne = GetNumberFromConsole("Enter the number: ");
+int i = 1;
+while (i <= numberOne)
+{
+Console.WriteLine(i);
+i =  i + 2;    
+}
