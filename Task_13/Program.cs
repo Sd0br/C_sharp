decff9dd6ec029e15bcad527ce8adae20486ecd0
Igor Yakushev <igor.yakushev@gmail.com>
2022-12-20 20:03:26 +0300

    Finish task 13

---
+++ Task_13/Program.cs
@@ -0,0 +1,27 @@
+﻿//получает число с консоли. "Защита от дурака"
+int GetNumberFromConsole(string message)
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.Write(message);
+
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число.");
+        }
+    }
+
+    return result;
+}
+
+int number = GetNumberFromConsole("Enter the number: ");
+
+double countOfNumber = Math.Floor(Math.Log10(number) + 1);
+Console.WriteLine(countOfNumber);
+
