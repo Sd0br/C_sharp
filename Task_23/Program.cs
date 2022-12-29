f2c1b9346bd99a8848581b7d68e717e807814907
Igor Yakushev <igor.yakushev@gmail.com>
2022-12-29 13:40:04 +0300

    Finish task 23

---
+++ Task_23/Program.cs
@@ -0,0 +1,33 @@
+﻿//Задача 23
+//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+//получает число с консоли. "Защита от дурака"
+int GetNumberFromConsole(string message)
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.Write(message);
+
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Input not a number.");
+        }
+    }
+
+    return result;
+}
+
+int NumberOne = GetNumberFromConsole("Input number: ");
+int Index = 1;
+
+while (Index <= NumberOne)
+{
+    Console.WriteLine(Math.Pow(Index, 3));
+    Index++;
+}
