ff8dc461588636ddf73fcefb08613a1d5bcd9acc
Igor Yakushev <igor.yakushev@gmail.com>
2022-12-29 12:37:17 +0300

    Finish task 19

--- Exersise1_1/Program.cs
+++ Exersise1_1/Program.cs
@@ -1,2 +1,43 @@
-﻿Console.Write("Input number 1: ");
+﻿int GetNumber(string message)
+{
+    int result = 0;
+
+    while (true)
+    {
+        Console.Write(message);
+
+        if(int.TryParse(Console.ReadLine(), out result) && result != 0)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Entered not a number or zero. Try again.");
+        }
+    }
+
+    return result;
+}
+
+//int NumberOne = GetNumber("Input a number. More than zero.");
+//Console.WriteLine(NumberOne);
+
+int GetQuarter(int x, int y)
+{
+    if (x > 0 && y > 0)
+        return 1;
+    else if (x < 0 && y > 0)
+        return 2;
+    else if (x < 0 && y < 0)
+        return 3;
+    else
+        return 4;
+}
+
+int x = GetNumber("Input coordinate X, not equel zero ");
+int y = GetNumber("Input coordinate Y, not equel zero ");
+int quarter = GetQuarter(x, y);
+
+Console.WriteLine($"Point with coordinates ({x}, {y}) belongs quarter {quarter}");
+
 
---
+++ Task_19/Program.cs
@@ -0,0 +1,52 @@
+﻿//Задача 19
+//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+//получает число с консоли. "Защита от дурака"
+int GetNumberFromConsole(string message)
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.Write(message);
+
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Input not a number.");
+        }
+    }
+
+    return result;
+}
+
+string NumberToString = (GetNumberFromConsole("Input number: ")).ToString();
+
+int NumberLength = NumberToString.Length;
+int index = 0;
+bool Flag = true;
+
+while (index < NumberLength / 2)
+{
+//Console.WriteLine(index);
+if (NumberToString[index] != NumberToString[NumberLength - index -1])
+    {
+        Flag = false;
+        break;
+    }
+index++;  
+}
+
+if (Flag)
+    {
+        Console.WriteLine($"The number {NumberToString} is a palindrome");
+    }
+else
+    {
+        Console.WriteLine($"The number {NumberToString} is not a palindrome");
+    }
+
+
