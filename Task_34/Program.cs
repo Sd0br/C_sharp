54318f8f2ff81cadc2c1803acbf9a0e0499279ba
Igor Yakushev <igor.yakushev@gmail.com>
2023-01-16 12:29:46 +0300

    Finish task 34

---
+++ Task_34/Program.cs
@@ -0,0 +1,50 @@
+﻿int GetNumber(string message)
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.Write(message);
+
+        if(int.TryParse(Console.ReadLine(), out result) && result >= 1)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Entered not a number or less than 1");
+        }
+
+    }
+
+    return result;
+}
+
+int[] NewArray(int elementsNumber)
+{
+    int[] newMassive = new int[elementsNumber];
+    Random rnd = new Random(); 
+
+    for (int i = 0; i < elementsNumber; i++)
+    {
+        newMassive[i] = rnd.Next(100, 1000);
+        Console.WriteLine(newMassive[i]);
+    }
+    return newMassive;
+}
+
+int countElement = GetNumber("Enter number of array's elements ");
+int[] ourArray = NewArray(countElement);
+
+
+int NumberOfEvenNumbers = 0;
+for (int i = 0; i < countElement; i++)
+{
+    if (ourArray[i] % 2 == 0)
+    {
+        NumberOfEvenNumbers++;
+    }
+}
+
+Console.Write($"Number of even numbers in arrais is {NumberOfEvenNumbers}");
+
--- Temp/Program.cs
+++ Temp/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result) && result >= 1)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число или меньше 1");
+        }
+
+    }
+
+    return result;
+
+}
+
+int GetSumm(int number)
+{
+    int summ = 0;
+
+    for (int i = 0; i <= number; i++)
+    {
+        summ += i;
+    }
+
+    return summ;
+}
+
+int number = GetNumber("Введите число больше 1");
+int summ = GetSumm(number);
+
+Console.WriteLine($"Сумма чисел от 1 до  {number} = {summ}");
+*/
+
+Random rnd = new Random();
+for (int i = 0; i < 100; i++)
+{
+    int a = rnd.Next(100, 1000);  
+    Console.WriteLine(a);
+}
+
+
+
+
