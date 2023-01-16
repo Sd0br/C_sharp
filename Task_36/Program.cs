3d1db8999c025426ca1120da70a4d96976b5e379
Igor Yakushev <igor.yakushev@gmail.com>
2023-01-16 12:45:39 +0300

    Finish task 36

---
+++ Task_36/Program.cs
@@ -0,0 +1,45 @@
+﻿int GetNumber(string message)
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.Write(message);
+
+        if(int.TryParse(Console.ReadLine(), out result) && result >= 1)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Entered not a number or less than 1");
+        }
+
+    }
+
+    return result;
+}
+
+int[] NewArray(int elementsNumber)
+{
+    int[] newMassive = new int[elementsNumber];
+    Random rnd = new Random(); 
+
+    for (int i = 0; i < elementsNumber; i++)
+    {
+        newMassive[i] = rnd.Next();
+        Console.WriteLine(newMassive[i]);
+    }
+    return newMassive;
+}
+
+int countElement = GetNumber("Enter number of array's elements ");
+int[] ourArray = NewArray(countElement);
+
+double summOfOddElements = 0;
+for (int i = 0; i < countElement; i = i + 2)
+{
+    summOfOddElements = summOfOddElements + ourArray[i];
+}
+
+Console.WriteLine($"Summ of odd elements is {summOfOddElements}");
