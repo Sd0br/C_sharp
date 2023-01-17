4254dafc6a86135202d9bd37f543a09207ed5de0
Igor Yakushev <igor.yakushev@gmail.com>
2023-01-17 12:58:45 +0300

    Finish task 25

---
+++ Task_25/Program.cs
@@ -0,0 +1,35 @@
+﻿/* Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.Write(message);
+
+        if(int.TryParse(Console.ReadLine(), out result) && result >= 1)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Entered not a number or less than 1");
+        }
+
+    }
+
+    return result;
+}
+
+int a = GetNumber("Enter number A: ");
+int b = GetNumber("Enter number B: ");
+int factor = a;
+for (int i = 1; i < b; i++)
+{
+    a = a * factor;
+}
+ Console.WriteLine(a);
