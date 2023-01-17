0d4210f0779abd42812584dea614668b21de0903
Igor Yakushev <igor.yakushev@gmail.com>
2023-01-17 13:55:15 +0300

    Finish task 29

---
+++ Task_29/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+все 8 элементов должны получить за один Console.ReadLine();
+подсказка: использовать потом метод string.Split();
+*/
+
+Console.Write("Введите 8 чисел через ,: ");
+string mainString = Console.ReadLine();
+string[] twoString = mainString.Split(",");
+//Console.WriteLine(twoString.Length);
+int[] newArray = new int[twoString.Length];
+for (int i = 0; i < twoString.Length; i++)
+{
+     //Console.WriteLine(twoString[i]);
+     int.TryParse(twoString[i], out newArray[i]);
+}
+
+for (int i = 0; i < twoString.Length; i++)
+{
+     Console.WriteLine(newArray[i]);
+}
