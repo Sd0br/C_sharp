71a85eb3cab438818e8135fee9977d57149dfe07
Igor Yakushev <igor.yakushev@gmail.com>
2023-01-24 10:05:53 +0300

    Finish task 41

---
+++ Task_41/Program.cs
@@ -0,0 +1,46 @@
+﻿/*
+Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+
+
+int PositiveNumbersInArray(int[] array)
+
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            result++;
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+Console.Write("Enter multiple numbers through ,: ");
+string mainString = Console.ReadLine();
+string[] twoString = mainString.Split(",");
+//Console.WriteLine(twoString.Length);
+int[] newArray = new int[twoString.Length];
+for (int i = 0; i < twoString.Length; i++)
+{
+     //Console.WriteLine(twoString[i]);
+     int.TryParse(twoString[i], out newArray[i]);
+}
+
+PrintArray(newArray);
+Console.Write("Positive numbers in array is: ");
+Console.WriteLine(PositiveNumbersInArray(newArray));
+
+
--- Temp/Program.cs
+++ Temp/Program.cs
@@ -38,7 +38,7 @@ int number = GetNumber("Введите число больше 1");
 int summ = GetSumm(number);
 
 Console.WriteLine($"Сумма чисел от 1 до  {number} = {summ}");
-*/
+
 
 Random rnd = new Random();
 for (int i = 0; i < 100; i++)
@@ -49,4 +49,69 @@ for (int i = 0; i < 100; i++)
 
 
 
+int[] InitArray(int dimenssion)
+{
+    int[] arr = new int[dimenssion];
+    Random rnd = new Random();
+    for (int i = 0; i < dimenssion; i++)
+    {
+        arr[i] = rnd.Next(1,10);
+    }
+
+    return arr;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int [] ReverseArray(int[] array)
+
+{
+    int[] result = new int[array.Length];
+    int count = 0;
+    for (int i = array.Length - 1; i >= 0; i--)
+    {
+        result[count] = array[i];
+        count++;
+    }
+    return result;
+}
+
+Console.WriteLine("Введите размерность массива");
+int dimenssion = int.Parse(Console.ReadLine());
+int[] arr = InitArray(dimenssion);
+int[] reverseArr = ReverseArray(arr);
+
+PrintArray(arr);
+
+PrintArray(reverseArr);
+
+*/
+
+Console.WriteLine("Input a number");
+int dimenssion = int.Parse(Console.ReadLine());
+int[] ourArray =new int[dimenssion];
+ourArray[0] = 0;
+ourArray[1] = 1;
+for (int i = 2; i < dimenssion; i++)
+{
+    ourArray[i] = ourArray[i - 1] + ourArray[i - 2];
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+PrintArray(ourArray);
 
