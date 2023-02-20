0c2d0b64a8c4ddd3ccdb5f8e1e62ee7e8f3ff768
Igor Yakushev <igor.yakushev@gmail.com>
2023-02-20 11:27:40 +0300

    Print table

--- itog/Program.cs
+++ itog/Program.cs
@@ -1,3 +1,18 @@
 ﻿// формирует из исходного массива строк, массив длина элементов которого 3 или меньше символа
 
+void PrintTable(string[] tab)
+{
+    for (int i = 0; i < tab.GetLength(0); i++)
+    {
+        Console.Write($"{tab[i]} ");
+    }
+}
+
+
+string[] firstArray = {"123", "asdfsasdf", "12345"};
+
+PrintTable(firstArray);
+
+
+
 
